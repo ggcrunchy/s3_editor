0368801df3f5c2ccfcf7b1c7408a03b7519dbe01
Steven Johnson <steve@xibalbastudios.com>
2015-05-05 16:05:06 -0500

    Minor advances on choose audio overlay (running up against shortcomings in layout module, as far as porting goes)

--- overlay/ChooseAudio.lua
+++ overlay/ChooseAudio.lua
@@ -23,12 +23,62 @@
 -- [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
 --
 
+-- Modules --
+local net = require("corona_ui.patterns.net")
+local touch = require("corona_ui.utils.touch")
+
+-- Corona globals --
+local display = display
+
 -- Corona modules --
 local composer = require("composer")
 
 -- Choose audio overlay --
 local Overlay = composer.newScene()
 
--- STUFF!
+-- Drag touchy listener
+local DragTouch = touch.DragParentTouch()
+
+--
+local function Backdrop (group, w, h, corner)
+	local backdrop = display.newRoundedRect(group, 0, 0, w, h, corner)
+
+	backdrop:addEventListener("touch", DragTouch)
+	backdrop:setFillColor(.375, .675)
+	backdrop:setStrokeColor(.125)
+	backdrop:translate(w / 2, h / 2)
+
+	backdrop.strokeWidth = 2
+
+	return backdrop
+end
+
+--
+function Overlay:create ()
+	net.Blocker(self.view)
+
+	Backdrop(self.view, 350, 350, 22)
+	-- ^^^ Problem: layout modules not robust for dialogs (yet...)
+end
+
+Overlay:addEventListener("create")
+
+--
+function Overlay:show (event)
+	if event.phase == "did" then
+		--
+	end
+end
+
+Overlay:addEventListener("show")
+
+--
+function Overlay:hide (event)
+	if event.phase == "did" then
+		--
+	end
+end
+
+Overlay:addEventListener("hide")
 
 return Overlay
