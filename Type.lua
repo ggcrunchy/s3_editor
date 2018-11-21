f59528c9b3868e9505ad870f7ac522309d53c6c5
Steven Johnson <steve@xibalbastudios.com>
2018-11-21 16:31:52 -0600

    Teasing out a bit what Type should be and the VERY rough beginnings of a LinkableType base built from that

--- Type.lua
+++ Type.lua
@@ -28,533 +28,171 @@
 
 -- Standard library imports --
 local assert = assert
-local concat = table.concat
-local ipairs = ipairs
 local pairs = pairs
-local setmetatable = setmetatable
 local type = type
 
 -- Modules --
 local adaptive = require("tektite_core.table.adaptive")
-local array_funcs = require("tektite_core.array.funcs")
-local iterator_utils = require("iterator_ops.utils")
 
--- Unique member keys (Sublink) --
-local _can_link = {}
-local _interfaces = {}
-local _link_to = {}
-local _name = {}
-local _template = {}
+-- Exports --
+local M = {}
 
--- Unique member keys (Tags) --
-local _implemented_by = {}
-local _implies = {}
+--
+--
+--
 
--- Iterator over a list of strings (tags or sublinks)
-local IterStrList = iterator_utils.InstancedAutocacher(function()
-	local str_list = {}
+local State = {}
 
-	-- Body --
-	return function(_, i)
-		return i + 1, str_list[i + 1]
-	end,
-
-	-- Done --
-	function(_, i)
-		return str_list[i + 1] == nil
-	end,
-
-	-- Setup --
-	function(T, enum, name_, as_set, filter)
-		-- Build up the list of strings, accommodating the form of the source.
-		local count = 0
-
-		if as_set then
-			for base in adaptive.IterSet(name_) do
-				count = enum(T, str_list, base, count, filter)
-			end
-		else
-			for _, base in adaptive.IterArray(name_) do
-				count = enum(T, str_list, base, count, filter)
-			end
-		end
-
-		-- Enumeration will overwrite the old elements, but if the previous iteration was
-		-- longer than this one, the list will still contain leftover elements at the tail
-		-- end, so trim the list as needed. Remove any duplicates to get the final list.
-		for i = #str_list, count + 1, -1 do
-			str_list[i] = nil
-		end
-
-		array_funcs.RemoveDups(str_list)
-
-		return nil, 0
-	end
-end)
-
--- Tags class definition --
---return class.Define(function(Tags)
-
-local Type = {}
-
-Type.__index = Type
-
-	-- Forward references --
-	local GetTemplate, ReplaceSingleInstance
-
-local function IsTemplate (name)
-	return type(name) == "string" and name:sub(-1) == "*"
-end
-
-	-- Nothing to iterate
-	local function NoOp () end
-
-	-- Iterate pairs()-style, if the table exists
-	local function Pairs (t)
-		if t then
-			return pairs(t)
-		else
-			return NoOp
-		end
-	end
-
-	do
-		--
-		local function AuxHasSublink (T, name, sub)
-			--
-			local tag = T[_tags][name]
-			local sub_links = tag.sub_links
-
-			if sub_links then
-				local instances = tag.instances
-				local sublink = sub_links[sub] or (instances and instances[sub])
-
-				if sublink then
-					return sublink, sub_links, instances
-				end
-			end
-		end
-
-		-- --
-		local Name1, Sub1, Sublink1, SublinksList1, InstancesList1
-		local Name2, Sub2, Sublink2, SublinksList2, InstancesList2
-
-		--
-		local function FindSublink (T, name, sub)
-			if name == Name1 and sub == Sub1 then
-				return Sublink1, SublinksList1, InstancesList1
-			elseif name == Name2 and sub == Sub2 then
-				return Sublink2, SublinksList2, InstancesList2
-			else
-				local sublink, slist, ilist = AuxHasSublink(T, name, sub)
-
-				Name1, Sub1, Sublink1, SublinksList1, InstancesList1 = name, sub, sublink, slist, ilist
-				Name2, Sub2, Sublink2, SublinksList2, InstancesList2 = Name1, Sub1, Sublink1, SublinksList1, InstancesList1
-
-				return sublink, slist, ilist
-			end
-		end
-
-		--- DOCME
-		function Tags:CanLink (name1, name2, object1, object2, sub1, sub2, arg)
-			local is_cont, why = true
-
-			if IsTemplate(sub1) then
-				why = "Sublink #1 is a template: `" .. sub1 .. "`"
-			elseif IsTemplate(sub2) then
-				why = "Sublink #2 is a template: `" .. sub2 .. "`"
-			else
-				local so1 = FindSublink(self, name1, sub1)
-
-				if so1 then
-					local so2 = FindSublink(self, name2, sub2)
-
-					if so2 then
-						local passed = true
-
-						for _, can_link in adaptive.IterArray(so1[_can_link]) do
-							passed, why, is_cont = can_link(object1, object2, so1, so2, arg)
-
-							if not passed then
-								break
-							end
-						end
-
-						if passed then
-							return true
-						end
-					else
-						why = "Missing sublink #2: `" .. (sub2 or "?") .. "`"
-					end
-				else
-					why = "Missing sublink #1: `" .. (sub1 or "?") .. "`"
-				end
-			end
-
-			return false, why or "", not not is_cont
-		end
--- ^^ sort of used elsewhere
 --- DOCME
-function Type:GetTemplate (name)
-	local pi, nodes = name:find("|"), self.m_nodes
-	local template = (pi and nodes) and name:sub(1, pi - 1) .. "*"
+function M.GetState (name)
+	assert(name ~= nil, "Invalid type name")
 
-	return nodes[template] and template
+	local state = State[name] or {}
+
+	State[name] = state
+
+	return state
 end
 
-local Node = {}
+local Reserved = { _before = true, _instead = true, _list = true, _name = true, _prototype = true }
 
-Node.__index = Node
-
---- Getter.
--- @treturn string Name.
-function Node:GetName ()
-	return self[_name]
+local function MergePrototype (def, proto)
+	for k, v in pairs(proto) do
+		if k == "_list" and def._list then
+			MergePrototype(def._list, v) -- will both be tables without own _list member
+		elseif def[k] == nil then -- not defined in new type, including not extending anything in prototype
+			def[k] = v
+		end
+	end
 end
 
-		--- Predicate.
-		-- @param name
-		-- @string sub
-		-- @param what
-		-- @treturn boolean X
-		function Tags:ImplementedBySublink (name, sub, what)
-			local sub_link = FindSublink(self, name, sub)
+local function PrototypeEntry (proto, name)
+	local list = proto._list
+	local entry = list and list[name] or proto[name] -- n.b. fallthrough when list[name] nil
 
-			return sub_link ~= nil and sub_link:Implements(what)
-		end
--- ^^ one use in link.lua
+	return adaptive.IterArray(entry)
+end
 
-	end
+local function AddCallAfterProtoCalls (def, name, func, proto)
+	local arr = def[name]
 
-	do
-		-- Sublink class definition --
-		local SublinkClass = class.Define(function(Sublink)
-
-
-			--- Predicate.
-			-- @param what
-			-- @treturn boolean X
-			function Sublink:Implements (what)
-				return adaptive.InSet((self[_template] or self)[_interfaces], what)
-			end
--- ^^ used here...
-			--- Class cloner.
-			-- @string name Instance name.
-			function Sublink:__clone (S, name)
-				for _, can_link in adaptive.IterArray(S[_can_link]) do
-					self[_can_link] = adaptive.Append(self[_can_link], can_link)
-				end
-
-				for _, link_to in adaptive.IterArray(S[_link_to]) do
-					self[_link_to] = adaptive.Append(self[_link_to], link_to)
-				end
-
-				self[_name], self[_template] = name, S
-			end
-
-			--- Class constructor.
-			-- @string name Sublink name.
-			function Sublink:__cons (name)
-				self[_name] = name
-			end
-		end)
-			
-		--
-		local function AddInterface (sub, what)
-			adaptive.AddToSet_Member(sub, _interfaces, what)
-		end
-
-		--
-		local function CanLinkTo (_, _, sub, other_sub)
-			local link_to = sub[_link_to]
-
-			for _, what in adaptive.IterArray(link_to) do
-				if other_sub:Implements(what) then
-					return true
-				end
-			end
-
-			local list, names
-
-			for _, what in adaptive.IterArray(link_to) do
-				names = adaptive.Append(names, what)
-			end
-
-			if type(names) == "table" then
-				list = "`" .. concat(names, "` or `") .. "`"
-			else
-				list = "`" .. names .. "`" -- known to contain at least one
-			end
-
-			return false, "Expected " .. list, true
-		end
-
-		--- DOCME
-		-- @param name
-		-- @param what
-		function Tags:ImplyInterface (name, what)
-			adaptive.AddToSet_Member(self[_implies], name, what)
-		end
--- used for type reciprocity...
-		--
-		local function AddImplementor (T, name, what)
-			local implemented_by = T[_implemented_by]
-
-			for impl_by in adaptive.IterSet(implemented_by[what]) do
-				--[[
-				if Is(T, name, impl_by) then
-					return
-				end
-				]]
-			end
-
-			adaptive.AddToSet_Member(implemented_by, what, name)
-		end
-
-		--- DOCME
-		-- @string name
-		-- @ptable[opt] options
-		function Tags:New (name, options)
-			local tags = self[_tags]
-
-			assert(not tags[name], "Tag already exists")
-
-			local tag, new = {}
-
-			if options then
-				-- We track the tag's parent and child tag names, so that these may be iterated.
-				-- The parents are only assigned at tag creation, so we can safely put these at
-				-- the beginning of the tag's info array; whereas child tags may be added over
-				-- time. By making note of how many parents there were, however, we can append
-				-- the children to the same array: namely, the new tag name itself is here added
-				-- to each of its parents.
-				for _, pname in ipairs(options) do
-					local ptag = assert(tags[pname], "Invalid parent")
-
-					assert(ptag[#ptag] ~= name, "Duplicate parent")
-
-					ptag[#ptag + 1], tag[#tag + 1] = name, pname
-				end
-
-				-- Add any sublinks.
-				local sub_links, implies = options.sub_links, self[_implies]
-
-				if sub_links then
-					local new = {}
-
-					for name, sub in pairs(sub_links) do
-						local stype, obj, link_to = type(sub), SublinkClass(name)
-
-						--
-						if type(name) == "string" then
-							assert(name:find("|") == nil, "Pipes are reserved for instanced templates")
-							assert(name:find(":") == nil, "Colons are reserved for compound IDs")
-
-							if name:sub(-1) == "*" and not tag.instances then
-								self.counters, tag.instances = self.counters or {}, {}
-							end
-						end
-
-						--
-						if stype == "table" then
-							for _, v in ipairs(sub) do
-								AddInterface(obj, v)
-							end
-
-							--
-							link_to = sub.link_to
-
-						--
-						elseif sub then
-							link_to = sub
-						end
-						
-						--
-						local found_string
-
-						for _, what in adaptive.IterArray(link_to) do
-							local wtype = type(what)
-
-							if wtype == "string" then
-								if not found_string then
-									obj[_can_link], found_string = adaptive.Append(obj[_can_link], CanLinkTo), true
-								end
-
-								obj[_link_to] = adaptive.Append(obj[_link_to], what)
-
-								--
-								for interface in adaptive.IterSet(implies[what]) do
-									AddInterface(obj, interface)
-								end
-
-							--
-							elseif wtype == "function" then
-								obj[_can_link] = adaptive.Append(obj[_can_link], what)
-							end
-						end
-
-						--
-						new[name] = obj
-					end
-
-					tag.sub_links = new
-				end
-
-				--
-				for _, sub in Pairs(new) do
-					for what in adaptive.IterSet(sub[_interfaces]) do
-						AddImplementor(self, name, what)
-					end
-				end
-			end
-
-			--
-			tags[name], tag.nparents = tag, #(options or "")
+	if not arr then -- prototype calls not already merged by "before" logic?
+		for _, ev in PrototypeEntry(proto, name) do
+			arr = adaptive.Append(arr, ev)
 		end
 	end
 
-	do
-		--
-		local Template
+	def[name] = adaptive.Append(arr, func)
+end
 
-		local function GeneratedFrom (name)
-			local where = name:find("|")
+local function AddCallBeforeProtoCalls (def, name, func, proto)
+	local arr = adaptive.Append(nil, func)
 
-			return where and name:sub(where - 1) == Template
-		end
+	for _, ev in PrototypeEntry(proto, name) do
+		arr = adaptive.Append(arr, ev)
+	end
 
-		--
-		local Filters = {
-			instances = function(name)
-				return name:sub(-1) == "|"
-			end,
+	def[name] = arr
+end
 
-			no_instances = function(name)
-				return name:sub(-1) ~= "|"
-			end,
+local function AddCallDirectly (def, name, func)
+	def[name] = func
+end
 
-			no_templates = function(name)
-				return name:sub(-1) ~= "*"
-			end,
+local function WrapCallLists (def)
+	local list
 
-			templates = function(name)
-				return name:sub(-1) == "*"
-			end
-		}
+	for k, v in pairs(def) do
+		if type(v) == "table" then
+			list = list or {} -- only create if needed
 
-		--
-		local function EnumSublinks (T, str_list, name, count, filter)
-			--
-			local tag, was = T[_tags][name], count
-
-			for _, v in Pairs(tag.sub_links) do
-				str_list[count + 1], count = v:GetName(), count + 1
-			end
-
-			for name in Pairs(tag.instances) do
-				str_list[count + 1], count = name, count + 1
-			end
-
-			--
-			if filter then
-				for i = count, was + 1, -1 do
-					if not filter(str_list[i]) then
-						str_list[i] = str_list[count]
-						count, str_list[count] = count - 1
-					end
+			local function wrapped (event)
+				for i = 1, #v do
+					v[i](event)
 				end
 			end
 
-			return count
+			wrapped[k], def[k] = wrapped
 		end
+	end
 
-		--- DOCME
-		-- @string name
-		-- @string[opt] filter
-		-- @treturn iterator I
-		function Tags:Sublinks (name, filter)
-			if filter then
-				if IsTemplate(filter) then
-					filter, Template = GeneratedFrom, filter:sub(1, -2)
-				else
-					filter = Filters[filter]
-				end
+	def._list = list
+end
+
+local function AddNewCalls (def, params, add, proto)
+	for k, v in pairs(params) do
+		if not Reserved[k] then
+			add(def, k, v, proto)
+		end
+	end
+
+	WrapCallLists(def)
+end
+
+local Types = {}
+
+--- DOCME
+-- @ptable params
+-- @return N
+function M.NewType (params)
+	assert(type(params) == "table", "Invalid params")
+
+	local name = params._name
+
+	assert(name ~= nil and name == name, "Invalid name")
+	assert(not Types[name], "Name already in use")
+
+	local before, instead, pname, def = params._before, params._instead, params._prototype, {}
+
+	if pname == nil then
+		assert(not before, "Prototype must be available for `before` calls")
+		assert(not instead, "Prototype must be available for `instead` calls")
+
+		AddNewCalls(def, params, AddCallDirectly)
+	else
+		local proto = assert(Types[pname], "Invalid prototype")
+
+		if instead then
+			assert(not instead._pre_init, "Instead list may not contain `pre_init` call")
+
+			for k, v in pairs(instead) do
+				assert(not (before and before[k] ~= nil), "Entry in `instead` also in `before`")
+				assert(params[k] ~= nil, "Entry in `instead` also in main list")
+
+				def[k] = v
 			end
+		end
 
-			return IterStrList(self, EnumSublinks, name, false, filter)
+		if before then
+			assert(not before._pre_init, "Before list may not contain `pre_init` call")
+
+			for k, v in pairs(before) do
+				AddCallBeforeProtoCalls(def, k, v, proto)
+			end
+		end
+
+		AddNewCalls(def, params, AddCallAfterProtoCalls, proto)
+		MergePrototype(def, proto)
+	end
+
+	local pre_init, init = def._pre_init, def._init
+
+	if pre_init or init then
+		if pre_init then
+			pre_init(name)
+		end
+
+		if init then
+			init(name)
 		end
 	end
 
-	--- Class constructor.
-	function Tags:__cons ()
-		self[_implemented_by] = {}
-		self[_implies] = {}
-	end
+	Types[name] = def
 
-	-- Bind references.
-	GetTemplate, ReplaceSingleInstance = Tags.GetTemplate, Tags.ReplaceSingleInstance
-end)
+	return name
+end
 
--- Type:Iter (...)
---  IterTemplates (...)
---  IterNonTemplates (...)
+-- TODO: stuff to apply it...
 
--- M.EnumNamesGeneratedFrom (template, list) -- "instances"
--- M.EnumNamesNotGeneratedFrom (template, list) -- "no_instances"
--- M.NewType ({ methods, before, instead }, { export_nodes }, { import_nodes })
--- M.ReplaceGeneratedName (list, name) -- need type?
--- M.ReplaceNames...
--- M.RemoveGeneratedName (...) -- ????
--- M.GenerateName (list, template)
-
-	--- DOCME
-	-- @string name
-	-- @string sub
-	-- @treturn ?|string|nil X
-	function Tags:Instantiate (name, sub)
-		if IsTemplate(sub) then
-			local template, _, ilist = FindSublink(self, name, sub)
-			local id = (self.counters[sub] or 0) + 1
-			local instance = ("%s|%i|"):format(sub:sub(1, -2), id)
-
-			ilist[instance], self.counters[sub] = class.Clone(template, instance), id
-
-			return instance
-		else
-			return nil
-		end
-	end
-
-	--- DOCME
-	-- @string name
-	-- @string instance
-	-- @treturn boolean X
-	function Tags:Release (name, instance)
-		local sublink, _, ilist = FindSublink(self, name, instance)
-
-		if sublink then
-			ilist[instance] = nil
-
-			return true
-		else
-			return false
-		end
-	end
-
-	--- DOCME
-	function Tags:ReplaceInstances (tag, instances)
-		local replacements = {}
-
-		for k in Pairs(instances) do
-			replacements[k] = ReplaceSingleInstance(self, tag, k)
-		end
-
-		return replacements
-	end
-
-	--- DOCME
-	function Tags:ReplaceSingleInstance (tag, instance)
-		local template = GetTemplate(self, tag, instance)
-
-		return template and self:Instantiate(tag, template)
-	end
+-- Export the module.
+return M
